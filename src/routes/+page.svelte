296500ec81a82dff52d0fcebaf654332af71996e
Linimik <jameskimimk@gmail.com>
2022-10-11 13:33:50 -0400

    Added tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+	<h1>James Lin</h1>
+	<h2>Lorem Ipsum</h2>
+</div>
+
+<!-- <style lang="scss">
+	@import url("https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@800&display=swap");
+
+	.main {
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
+		width: 100vw;
+		position: absolute;
+		top: 0px;
+		left: 0px;
+		background-color: #222225;
+	}
+
+	.title {
+		font-family: "M PLUS Rounded 1c", sans-serif;
+		color: #ececec;
+		font-size: 5rem;
+		text-align: center;
+		margin-bottom: 1px;
+	}
+
+	.subtitle {
+		font-family: "M PLUS Rounded 1c", sans-serif;
+		color: #ececec;
+		font-size: 5rem;
+		text-align: center;
+		margin-top: 1px;
+	}
+</style> -->
