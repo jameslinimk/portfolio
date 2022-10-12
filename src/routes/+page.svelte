9f6e2b6aaabf14cac5c1f2106be4d2b8394b2183
Linimik <jameskimimk@gmail.com>
2022-10-12 11:03:47 -0400

    Working on front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,32 @@
-<div>
-	<h1>James Lin</h1>
-	<h2>Lorem Ipsum</h2>
+<script lang="ts">
+	let topbar: HTMLDivElement
+
+	// onMount(() => {
+	// 	const observer = new IntersectionObserver(
+	// 		([el]) => el.target.classList.toggle("is-pinned", el.intersectionRatio < 1),
+	// 		{ threshold: [1] }
+	// 	)
+
+	// 	observer.observe(topbar)
+	// })
+</script>
+
+<div class="absolute top-0 left-0 w-full h-full bg-[#222225]">
+	<div
+		bind:this={topbar}
+		class="w-full flex flex-col items-center justify-center bg-[#1d1d21] border-b-4 border-b-[#272729] p-3"
+	>
+		<h1 class="font-m-plus text-6xl text-[#ebebeb] font-bold">James Lin</h1>
+		<h2 class="font-mono text-2xl text-[#ebebeb] font-semibold">I am a programmer</h2>
+	</div>
+
+	<p class="font-m-plus text-xl text-[#ebebeb] mx-5 sm:mx-10 md:mx-20 mt-2">
+		Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
+		ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
+		dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
+		sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+		invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
+		justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+		ipsum dolor sit amet.
+	</p>
 </div>
-
-<!-- <style lang="scss">
-	@import url("https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@800&display=swap");
-
-	.main {
-		display: flex;
-		flex-direction: column;
-		height: 100vh;
-		width: 100vw;
-		position: absolute;
-		top: 0px;
-		left: 0px;
-		background-color: #222225;
-	}
-
-	.title {
-		font-family: "M PLUS Rounded 1c", sans-serif;
-		color: #ececec;
-		font-size: 5rem;
-		text-align: center;
-		margin-bottom: 1px;
-	}
-
-	.subtitle {
-		font-family: "M PLUS Rounded 1c", sans-serif;
-		color: #ececec;
-		font-size: 5rem;
-		text-align: center;
-		margin-top: 1px;
-	}
-</style> -->
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ["./src/**/*.{html,js,svelte,ts}"],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				"m-plus": ["'M PLUS Rounded 1c'", "sans-serif"]
+			}
+		}
 	},
 	plugins: []
 }
