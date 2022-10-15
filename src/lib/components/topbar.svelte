040ead6b6d88c2f16ebfd71d5446c86371e1a0cc
Jameslinimk <jameskimimk@gmail.com>
2022-10-15 19:44:44 -0400

    Extracted parts to individual files + topbar

---
+++ src/lib/components/topbar.svelte
@@ -0,0 +1,116 @@
+<script lang="ts">
+	import { onMount } from "svelte"
+
+	export let titles: string[]
+	export let github: string
+	export let discord: string
+
+	const wait = (time: number) => new Promise((res) => setTimeout(res, time))
+
+	let subIndex = 0
+	let subtitleText = titles[subIndex]
+	onMount(() => {
+		setInterval(async () => {
+			for (let i = 0, l = subtitleText.length; i < l; i++) {
+				subtitleText = subtitleText.slice(0, -1)
+				await wait(50)
+			}
+
+			await wait(1000)
+
+			const chars = titles[subIndex].split("")
+			for (const char of chars) {
+				subtitleText += char
+				await wait(50)
+			}
+
+			subIndex += 1
+			if (subIndex > titles.length - 1) subIndex = 0
+		}, 5000)
+	})
+
+	let topbar: HTMLDivElement
+	let pinned = false
+
+	onMount(() => {
+		const observer = new IntersectionObserver(([e]) => (pinned = e.intersectionRatio < 1), {
+			threshold: [1]
+		})
+		observer.observe(topbar)
+	})
+</script>
+
+<div
+	class="w-full flex flex-col items-center justify-center bg-[#1d1d21] border-b-4 border-b-[#272729] p-3 sticky -top-[0.01px] z-30 group transition-all"
+	bind:this={topbar}
+	class:topbar-pin={pinned}
+>
+	<h1
+		class="font-m-plus text-6xl text-[#ebebeb] font-bold transition-all text-center"
+		class:title-pin={pinned}
+	>
+		James Lin
+	</h1>
+	<h2
+		class="font-mono text-2xl text-[#ebebeb] font-semibold relative transition-all text-center"
+		class:subtitle-pin={pinned}
+	>
+		I am a {subtitleText}
+		<div class="bg-[#ebebeb] rounded-sm w-4 h-1 absolute -right-4 bottom-0 blinker" />
+	</h2>
+	<div class="flex items-center justify-center gap-1">
+		<a href="https://discord.com/users/{discord}" target="_blank">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="w-9 sm:w-11 hover:scale-105 fill-[#d8d8d8] hover:fill-[#ececec] transition-all"
+				viewBox="0 0 71 55"
+			>
+				<g clip-path="url(#clip0)">
+					<path
+						d="M60.1045 4.8978C55.5792 2.8214 50.7265 1.2916 45.6527 0.41542C45.5603 0.39851 45.468 0.440769 45.4204 0.525289C44.7963 1.6353 44.105 3.0834 43.6209 4.2216C38.1637 3.4046 32.7345 3.4046 27.3892 4.2216C26.905 3.0581 26.1886 1.6353 25.5617 0.525289C25.5141 0.443589 25.4218 0.40133 25.3294 0.41542C20.2584 1.2888 15.4057 2.8186 10.8776 4.8978C10.8384 4.9147 10.8048 4.9429 10.7825 4.9795C1.57795 18.7309 -0.943561 32.1443 0.293408 45.3914C0.299005 45.4562 0.335386 45.5182 0.385761 45.5576C6.45866 50.0174 12.3413 52.7249 18.1147 54.5195C18.2071 54.5477 18.305 54.5139 18.3638 54.4378C19.7295 52.5728 20.9469 50.6063 21.9907 48.5383C22.0523 48.4172 21.9935 48.2735 21.8676 48.2256C19.9366 47.4931 18.0979 46.6 16.3292 45.5858C16.1893 45.5041 16.1781 45.304 16.3068 45.2082C16.679 44.9293 17.0513 44.6391 17.4067 44.3461C17.471 44.2926 17.5606 44.2813 17.6362 44.3151C29.2558 49.6202 41.8354 49.6202 53.3179 44.3151C53.3935 44.2785 53.4831 44.2898 53.5502 44.3433C53.9057 44.6363 54.2779 44.9293 54.6529 45.2082C54.7816 45.304 54.7732 45.5041 54.6333 45.5858C52.8646 46.6197 51.0259 47.4931 49.0921 48.2228C48.9662 48.2707 48.9102 48.4172 48.9718 48.5383C50.038 50.6034 51.2554 52.5699 52.5959 54.435C52.6519 54.5139 52.7526 54.5477 52.845 54.5195C58.6464 52.7249 64.529 50.0174 70.6019 45.5576C70.6551 45.5182 70.6887 45.459 70.6943 45.3942C72.1747 30.0791 68.2147 16.7757 60.1968 4.9823C60.1772 4.9429 60.1437 4.9147 60.1045 4.8978ZM23.7259 37.3253C20.2276 37.3253 17.3451 34.1136 17.3451 30.1693C17.3451 26.225 20.1717 23.0133 23.7259 23.0133C27.308 23.0133 30.1626 26.2532 30.1066 30.1693C30.1066 34.1136 27.28 37.3253 23.7259 37.3253ZM47.3178 37.3253C43.8196 37.3253 40.9371 34.1136 40.9371 30.1693C40.9371 26.225 43.7636 23.0133 47.3178 23.0133C50.9 23.0133 53.7545 26.2532 53.6986 30.1693C53.6986 34.1136 50.9 37.3253 47.3178 37.3253Z"
+					/>
+				</g>
+				<defs>
+					<clipPath id="clip0">
+						<rect width="71" height="55" fill="white" />
+					</clipPath>
+				</defs>
+			</svg>
+		</a>
+		<a href="https://github.com/{github}" target="_blank">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="w-8 sm:w-10 m-2 z-10 fill-[#d8d8d8] hover:scale-105 hover:fill-[#ececec] transition-all"
+				viewBox="0 0 24 24"
+			>
+				<path
+					d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+				/>
+			</svg>
+		</a>
+	</div>
+</div>
+
+<style>
+	.blinker {
+		animation: blink 1s step-start infinite;
+	}
+
+	@keyframes blink {
+		50% {
+			opacity: 0;
+		}
+	}
+
+	.topbar-pin {
+		@apply max-h-fit p-1;
+	}
+
+	.title-pin {
+		@apply text-2xl;
+	}
+
+	.subtitle-pin {
+		@apply hidden;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-	import { onMount } from "svelte"
+	import Projects from "$lib/components/projects.svelte"
+	import Topbar from "$lib/components/topbar.svelte"
 
-	const projects = [
+	const projects: [name: string, description: string, image: string, href: string][] = [
 		[
 			"Chess engine",
 			"Lorem ipsum dolor sit amet ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo",
@@ -33,63 +34,11 @@
 			"https://www.google.com/"
 		]
 	]
-
-	const titles = ["programmer", "student", "learner"]
-	const wait = (time: number) => new Promise((res) => setTimeout(res, time))
-
-	let subIndex = 0
-	let subtitleText = titles[subIndex]
-	onMount(() => {
-		setInterval(async () => {
-			for (let i = 0, l = subtitleText.length; i < l; i++) {
-				subtitleText = subtitleText.slice(0, -1)
-				await wait(50)
-			}
-
-			await wait(1000)
-
-			const chars = titles[subIndex].split("")
-			for (const char of chars) {
-				subtitleText += char
-				await wait(50)
-			}
-
-			subIndex += 1
-			if (subIndex > titles.length - 1) subIndex = 0
-		}, 5000)
-	})
-
-	let topbar: HTMLDivElement
-	let title: HTMLHeadingElement
-	let subtitle: HTMLHeadingElement
-	onMount(() => {
-		const observer = new IntersectionObserver(
-			([e]) => {
-				topbar.classList.toggle("topbar-parent-hover", e.intersectionRatio < 1)
-				console.log("sticky", e.intersectionRatio < 1)
-			},
-			{ threshold: [1] }
-		)
-		observer.observe(topbar)
-	})
+	const titles = ["programmer", "student", "coder", "learner"]
 </script>
 
 <div class="absolute top-0 left-0 min-w-full min-h-full bg-[#222225] -z-50">
-	<div
-		class="w-full flex flex-col items-center justify-center bg-[#1d1d21] border-b-4 border-b-[#272729] p-3 sticky -top-[0.01px] z-30 group transition-all"
-		bind:this={topbar}
-	>
-		<h1 class="font-m-plus text-6xl text-[#ebebeb] font-bold transition-all" bind:this={title}>
-			James Lin
-		</h1>
-		<h2
-			class="font-mono text-2xl text-[#ebebeb] font-semibold relative transition-all"
-			bind:this={subtitle}
-		>
-			I am a {subtitleText}
-			<div class="bg-[#ebebeb] rounded-sm w-4 h-1 absolute -right-4 bottom-0 blinker" />
-		</h2>
-	</div>
+	<Topbar {titles} github="jameslinimk" discord="400029130219061260" />
 
 	<p class="font-m-plus text-xl text-[#ebebeb] mx-5 sm:mx-24 md:mx-32 mt-2">
 		Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt
@@ -104,70 +53,5 @@
 		est Lorem ipsum dolor sit amet.
 	</p>
 
-	<div class="flex flex-col justify-center items-center overflow-hidden">
-		<h1 class="font-m-plus font-bold text-3xl text-[#ebebeb] my-5">My Projects</h1>
-		<div class="text-center">
-			{#each projects as project}
-				<div
-					class="bg-[#2a2b2e] w-fit max-w-full sm:max-w-xs rounded-md shadow-black shadow-sm relative overflow-hidden hover:scale-110 transition-all cursor-pointer group hover:z-20 clear-left inline-block mx-3 mb-4"
-				>
-					<img
-						src={project[2]}
-						alt="chess"
-						class="w-full opacity-70 group-hover:opacity-100 transition-all"
-					/>
-
-					<div class="px-4 pt-4">
-						<h1
-							class="font-m-plus text-[#ebebeb] text-center text-2xl font-bold relative transition-all"
-						>
-							{project[0]}
-						</h1>
-
-						<p class="font-m-plus text-[#ebebeb] text-center relative transition-all">
-							{project[1]}
-						</p>
-					</div>
-
-					<div class="w-full flex m-2">
-						<a href={project[3]} target="_blank">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class="w-6 transition-all z-10 fill-[#ebebeb] hover:fill-[#d1c7c7] hover:scale-125"
-								viewBox="0 0 24 24"
-							>
-								<path
-									d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
-								/>
-							</svg>
-						</a>
-					</div>
-				</div>
-			{/each}
-		</div>
-	</div>
+	<Projects {projects} />
 </div>
-
-<style>
-	.blinker {
-		animation: blink 1s step-start infinite;
-	}
-
-	@keyframes blink {
-		50% {
-			opacity: 0;
-		}
-	}
-
-	.topbar-parent-hover {
-		@apply max-h-fit p-1;
-	}
-
-	.title-hover {
-		@apply text-2xl;
-	}
-
-	.subtitle-hover {
-		@apply hidden;
-	}
-</style>
