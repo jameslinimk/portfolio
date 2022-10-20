35ae348de28710416d6ebf1edc6c621671b3857d
Linimik <jameskimimk@gmail.com>
2022-10-20 09:09:24 -0400

    Working on looker, added texture

--- src/lib/components/looker.svelte
+++ src/lib/components/looker.svelte
@@ -8,9 +8,20 @@
 		type LookAt,
 		type ThrelteContext
 	} from "@threlte/core"
+	import { spring } from "svelte/motion"
 	import type { Writable } from "svelte/store"
 	import type { Camera } from "three"
-	import { BoxGeometry, MeshStandardMaterial, Plane, Raycaster, Vector3 } from "three"
+	import {
+		BoxGeometry,
+		MeshLambertMaterial,
+		Plane,
+		Raycaster,
+		sRGBEncoding,
+		TextureLoader,
+		Vector3
+	} from "three"
+
+	const scale = spring(1)
 
 	let ctx: ThrelteContext
 	let container: HTMLDivElement
@@ -39,23 +50,37 @@
 		intersectPoint.z = $camera.position.z
 		lookAt = intersectPoint
 	}
+
+	const loader = new TextureLoader()
+	loader.setPath("images/cube/")
+	const loadImage = (path: string) => {
+		const mat = loader.load(path)
+		mat.encoding = sRGBEncoding
+		return new MeshLambertMaterial({ map: mat })
+	}
+	const materials = ["px.png", "nx.png", "py.png", "ny.png", "pz.png", "nz.png"].map((path) =>
+		loadImage(path)
+	)
 </script>
 
 <svelte:window on:mousemove={onPointerMove} />
 
-<div class="w-96 h-96" bind:this={container}>
+<div bind:this={container} class="fixed bottom-2 left-2 w-24 h-24">
 	<Canvas bind:ctx>
 		<PerspectiveCamera position={{ x: 0, y: 0, z: 3 }} lookAt={{ x: 0, y: 0, z: 0 }} />
 
-		<DirectionalLight position={{ x: 3, y: 3, z: 3 }} />
-		<AmbientLight color={0xffffff} intensity={1.25} />
+		<DirectionalLight position={{ x: 0, y: 0, z: 3 }} />
+		<AmbientLight color={0x000000} intensity={1.25} />
 
 		<Mesh
+			scale={$scale}
 			interactive
-			material={new MeshStandardMaterial()}
+			material={materials}
 			position={{ x: 0, y: 0, z: 0 }}
 			geometry={new BoxGeometry(0.8, 0.8, 0.8)}
 			{lookAt}
+			on:pointerenter={() => ($scale = 1.5)}
+			on:pointerleave={() => ($scale = 1)}
 		/>
 	</Canvas>
 </div>
--- src/lib/components/topbar.svelte
+++ src/lib/components/topbar.svelte
@@ -55,12 +55,12 @@
 
 <!-- Div to detect if pinned -->
 <div class="min-w-full -top-[1px] left-0 z-40 sticky bg-red-500" bind:this={pinnedDiv} />
+<Looker />
 
 <div
 	class="min-w-full flex flex-col items-center justify-center bg-[#1d1d21] border-b-4 border-b-[#272729] p-3 top-0 sticky z-30 group transition-all"
 	class:topbar-pin={pinned}
 >
-	<Looker />
 	<a
 		href="/"
 		class="font-m-plus text-6xl text-[#ebebeb] font-bold transition-all text-center"
