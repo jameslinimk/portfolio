c200b44fd5f6811228689058e8e8628361b1cec8
Linimik <jameskimimk@gmail.com>
2022-10-21 12:25:25 -0400

    Have to fix ring

--- src/lib/components/space.svelte
+++ src/lib/components/space.svelte
@@ -8,12 +8,15 @@
 	} from "@threlte/core"
 	import { onMount } from "svelte"
 	import {
+		DoubleSide,
 		Mesh,
 		MeshBasicMaterial,
 		MeshPhongMaterial,
+		RingGeometry,
 		SphereGeometry,
 		sRGBEncoding,
-		TextureLoader
+		TextureLoader,
+		Vector3
 	} from "three"
 	import { clamp, randFloat, randInt } from "three/src/math/MathUtils.js"
 
@@ -25,6 +28,7 @@
 		rotateX: number
 		speed: number
 		radius: number
+		extras: Mesh[]
 	}
 	/* --------------------------------- Planets -------------------------------- */
 	onMount(() => {
@@ -33,29 +37,76 @@
 		/* ----------------------------- Texture loader ----------------------------- */
 		const loader = new TextureLoader()
 		loader.setPath("images/planets/")
-		const loadPlanet = (config: {
-			rotateY: number
-			rotateX: number
-			speed: number
-			radius: number
-			image: string
-		}) => {
+		const loadPlanet = (
+			config: {
+				rotateY: number
+				rotateX: number
+				speed: number
+				radius: number
+				image: string
+				ring?: string
+			},
+			delay: number
+		) => {
 			const map = loader.load(config.image)
 			map.encoding = sRGBEncoding
+
+			const extras: Mesh[] = []
+
+			const texture = config.ring ? loader.load(config.ring) : null
+			if (texture) {
+				console.log("Loading ring", config.ring)
+
+				texture.encoding = sRGBEncoding
+				const geometry = new RingGeometry(1000, 1, 20)
+				const pos = geometry.attributes.position
+				const v3 = new Vector3()
+				for (let i = 0; i < pos.count; i++) {
+					v3.fromBufferAttribute(pos, i)
+					geometry.attributes.uv.setXY(i, v3.length() < 4 ? 0 : 1, 1)
+				}
+
+				const material = new MeshBasicMaterial({
+					map: texture,
+					color: 0xffffff,
+					side: DoubleSide,
+					transparent: true
+				})
+				const mesh = new Mesh(geometry, material)
+
+				extras.push(mesh)
+			}
+
 			const geometry = new SphereGeometry(config.radius, 20, 20)
-			const material = new MeshPhongMaterial({ map })
+			const material = new MeshPhongMaterial({
+				map,
+				shininess: 0.1
+			})
 
 			const planet = new Mesh(geometry, material)
 
-			planet.position.x = Math.random() * 5000 - 500
-			planet.position.y = Math.random() * 5000 - 500
-			planet.position.z = -10000
+			const x = Math.random() * 5000 - 500
+			const y = Math.random() * 5000 - 500
+			;[...extras, planet].forEach((mesh) => {
+				mesh.position.x = x
+				mesh.position.y = y
+				mesh.position.z = -10000
+				mesh.visible = false
 
-			ctx.scene.add(planet)
-			planets.push({
-				mesh: planet,
-				...config
+				ctx.scene.add(mesh)
 			})
+
+			setTimeout(() => {
+				;[...extras, planet].forEach((mesh) => {
+					mesh.visible = true
+				})
+
+				planets.push({
+					mesh: planet,
+					extras,
+					...config
+				})
+			}, delay)
 		}
 
 		/* -------------------------- Stagger planet spawn -------------------------- */
@@ -65,41 +116,55 @@
 				rotateX: 0.01,
 				rotateY: 0.01,
 				speed: 10,
-				radius: 50
+				radius: 75
 			},
 			{
 				image: "mars.jpg",
 				rotateX: 0.01,
-				rotateY: 0.05,
+				rotateY: 0.015,
 				speed: 15,
-				radius: 25
+				radius: 50
 			},
 			{
 				image: "jupiter.jpg",
 				rotateX: 0.005,
 				rotateY: 0.005,
 				speed: 7,
-				radius: 75
+				radius: 150
+			},
+			{
+				image: "venus.jpg",
+				rotateX: 0.017,
+				rotateY: 0.017,
+				speed: 18,
+				radius: 30
+			},
+			{
+				image: "saturn.jpg",
+				ring: "saturn_rings.png",
+				rotateX: 0.01,
+				rotateY: 0.01,
+				speed: 0,
+				radius: 500
 			}
 		].forEach((config) => {
-			setTimeout(() => {
-				loadPlanet(config)
-			}, randInt(0, 5000))
+			loadPlanet(config, randInt(0, 5000))
 		})
 
 		/* ---------------------------- Animating planets --------------------------- */
 		const animate = () => {
 			planets.forEach((planet) => {
-				planet.mesh.position.z += planet.speed
-				planet.mesh.rotation.y += planet.rotateY
-				planet.mesh.rotation.x += planet.rotateX
-				if (planet.mesh.position.z > 5000) {
-					planet.mesh.position.z -= 10000
-					planet.mesh.position.x = Math.random() * 1000 - 500
-					planet.mesh.position.y = Math.random() * 1000 - 500
-					planet.speed = clamp(planet.speed + randFloat(-5, 5), 2, 25)
-					console.log({ speed: planet.speed })
-				}
+				;[...planet.extras, planet.mesh].forEach((mesh) => {
+					mesh.position.z += planet.speed
+					mesh.rotation.y += planet.rotateY
+					mesh.rotation.x += planet.rotateX
+					if (mesh.position.z > 5000) {
+						mesh.position.z -= 10000
+						mesh.position.x = Math.random() * 1000 - 500
+						mesh.position.y = Math.random() * 1000 - 500
+						planet.speed = clamp(planet.speed + randFloat(-5, 5), 2, 25)
+					}
+				})
 			})
 			requestAnimationFrame(animate)
 		}
