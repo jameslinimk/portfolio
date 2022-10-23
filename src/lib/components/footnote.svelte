0d05d9528180f0a963d11d8ad5c2c9d4de3293d4
Linimik <jameskimimk@gmail.com>
2022-10-23 12:17:08 -0400

    New tab on footnote

--- src/lib/components/footnote.svelte
+++ src/lib/components/footnote.svelte
@@ -1,8 +1,10 @@
 <div class="bg-[#101012] p-5 rounded-t-md">
 	<p class="text-[#ececec] font-m-plus text-xs">
 		Planet textures from
-		<a href="https://www.solarsystemscope.com/textures/" class="text-[#6293f3] underline"
-			>https://www.solarsystemscope.com/textures/</a
+		<a
+			href="https://www.solarsystemscope.com/textures/"
+			class="text-[#6293f3] underline"
+			target="_blank">https://www.solarsystemscope.com/textures/</a
 		>
 	</p>
 </div>
